45a13b4bdd69f67db404b1ce0b1f703b7f956ee4
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-28 00:44:14 +0600

    load detail id

--- scripts/app.js
+++ scripts/app.js
@@ -26,7 +26,6 @@ const displayPhones = (phones,dataLimit) => {
     }
     // console.log(phones);
     phones.forEach(phone => {
-
         const div = document.createElement('div');
         div.classList.add = 'col';
         div.innerHTML = `
@@ -35,6 +34,7 @@ const displayPhones = (phones,dataLimit) => {
         <div class="card-body">
             <h5 class="card-title">${phone.phone_name}</h5>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <button href="#" onclick="showDetails('${phone.slug}')" class="btn btn-primary">See Details</button>
         </div>
         </div>
         `
@@ -69,4 +69,15 @@ const spinnerLoad = isSpin => {
 document.getElementById('show-all-btn').addEventListener('click', function () {
     prosesAllData()
 })
+
+
+
+// show details
+const showDetails=async id=>{
+    const url=`https://openapi.programming-hero.com/api/phone/${id}`
+    const res=await fetch(url);
+    const data =await res.json();
+    console.log(data);
+}
+
 // loadPhones()
